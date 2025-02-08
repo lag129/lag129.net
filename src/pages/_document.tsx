95788ea0ec289add66af6e021bb3885607004778
lag129 <65485662+lag129@users.noreply.github.com>
2025-02-08 21:41:17 +0900

    fix: タイトルの変更をもとに戻す

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="ja" title="lag129.net">
+    <Html lang="ja">
       <Head />
       <body className="antialiased">
         <Main />
