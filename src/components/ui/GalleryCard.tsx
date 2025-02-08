1e4d6622d19a83bd6a871705e3bad0af77895a16
lag129 <65485662+lag129@users.noreply.github.com>
2025-02-08 14:08:53 +0900

    fix: Imageにsizesを追加

--- src/components/ui/GalleryCard.tsx
+++ src/components/ui/GalleryCard.tsx
@@ -7,7 +7,7 @@ interface GalleryCardProps {
 export const GalleryCard = ({ imgUrl }: GalleryCardProps) => {
   return (
     <div className="relative aspect-square">
-      <Image src={imgUrl} alt="Gallery Image" fill style={{ objectFit: "cover" }} priority={true} className="rounded-3xl" />
+      <Image src={imgUrl} alt="Gallery Image" fill sizes="(max-width: 768px) 100vw, 50vw" style={{ objectFit: "cover" }} priority={true} className="rounded-3xl" />
     </div>
   );
 };
