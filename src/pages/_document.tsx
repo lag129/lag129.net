1cfa199ed0233a100b868f03f7663d09a06b6a6c
lag129 <65485662+lag129@users.noreply.github.com>
2025-02-08 21:36:43 +0900

    fix: タイトルを追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="ja">
+    <Html lang="ja" title="lag129.net">
       <Head />
       <body className="antialiased">
         <Main />
