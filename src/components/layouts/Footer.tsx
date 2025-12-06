c40b61870a76dcc12bdd116a3e6dfb4c89ddc992
lag129 <65485662+lag129@users.noreply.github.com>
2025-12-07 00:24:21 +0900

    Footerを更新

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="text-center text-sm font-normal text-[#75796c]">
-      <p>Copyright © 2022-2024 lag129</p>
+      <p>Copyright © 2022-2025 lag129</p>
     </footer>
   );
 };
