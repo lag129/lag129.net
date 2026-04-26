5168841d3d55fa3184820566e4ac88aa5b593c41
lag129 <65485662+lag129@users.noreply.github.com>
2026-04-26 22:26:31 +0900

    リンクを更新

--- src/components/layouts/ProfileView.tsx
+++ src/components/layouts/ProfileView.tsx
@@ -10,9 +10,10 @@ export const ProfileView = () => {
       </div>
       <div className="mb-4 mt-6 block border-b pb-2 text-4xl font-semibold leading-tight">lag129</div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-1 lg:gap-4">
-        <LinkButton imgUrl="/github.svg" serviceName="GitHub" userId="lag129" href="https://github.com/lag129" />
-        <LinkButton imgUrl="/x.png" serviceName="Twitter" userId="@lag_129" href="https://twitter.com/lag_129" />
-        <LinkButton imgUrl="/mastodon.svg" serviceName="Mastodon" userId="@lag129" href="https://fedibird.com/@lag129" />
+        <LinkButton serviceName="GitHub" userId="lag129" href="https://github.com/lag129" />
+        <LinkButton serviceName="Blog" userId="blog.lag129.net" href="https://blog.lag129.net" />
+        <LinkButton serviceName="Misskey" userId="@lag129" href="https://misskey.lag129.net/@lag129" />
+        <LinkButton serviceName="X (Twitter)" userId="@lag_129" href="https://x.com/lag_129" />
       </div>
     </Card>
   );
--- src/components/ui/LinkButton.tsx
+++ src/components/ui/LinkButton.tsx
@@ -1,20 +1,17 @@
 import React from "react";
-import Image from "next/image";
 
 interface LinkButtonProps {
-  imgUrl: string;
   serviceName: string;
   userId: string;
   href: string;
 }
 
-export const LinkButton = ({ imgUrl, serviceName, userId, href }: LinkButtonProps) => {
+export const LinkButton = ({serviceName, userId, href }: LinkButtonProps) => {
   return (
     <div className="rounded-3xl bg-[#dce7c8]">
       <a href={href} className="block p-2">
         <div className="flex items-center gap-5">
-          <Image src={imgUrl} alt="Service Image" width={50} height={50} className="pl-4"/>
-          <div>
+          <div className="pl-3">
             <div className="font-bold">{serviceName}</div>
             <div className="text-sm font-medium text-[#75796c]">{userId}</div>
           </div>
